a2590efb2453db29ddff2dd9c7ea4d479b920e0a
ryannesso <ryantrololo10@gmail.com>
2024-07-14 14:17:04 +0200

    JRBT-0: added springboot skeleton to the project

---
+++ src/main/java/com/github/ryannesso/jrtb/JobRadarTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.ryannesso.jrtb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobRadarTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobRadarTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/ryannesso/jrtb/JobRadarTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ryannesso.jrtb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobRadarTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
