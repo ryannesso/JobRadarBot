537c9150d360abb2d59afa82ccd0a625a46f5f07
ryannesso <ryantrololo10@gmail.com>
2024-08-15 17:59:48 +0200

    Bug Fix Report: Description: Due to an outdated version of telegrambots-spring-boot-starter, it was impossible to add more than one user to the database. After updating, everything works as expected.

--- start.sh
+++ start.sh
@@ -13,8 +13,8 @@ docker-compose stop
 # Add environment variables
 export BOT_NAME=$1
 export BOT_TOKEN=$2
-export BOT_DB_USERNAME='prod_jobradarbot_bd'
-export BOT_DB_PASSWORD='pass_jrb_1'
+export BOT_DB_USERNAME='jobradarbot'
+export BOT_DB_PASSWORD='root'
 
 # Start new deployment
 docker-compose up --build -d
